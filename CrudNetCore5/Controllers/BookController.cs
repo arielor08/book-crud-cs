b90ca1e891cf3f18888b2d2cfa16d643d78843d6
arielort08 <ariel.ort08@hotmail.com>
2021-03-09 11:48:26 -0400

    Add project files.

---
+++ CrudNetCore5/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using CrudNetCore5.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CrudNetCore5/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="The Title is required")]
+        [StringLength(50,ErrorMessage ="The {0} must be at least {2} and max {1} characters",MinimumLength =3)]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "The Description is required")]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and max {1} characters", MinimumLength = 3)]
+        public string Description { get; set; }
+        [Required(ErrorMessage ="The Release Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "The Author is required")]
+
+        public string Author { get; set; }
+        [Required(ErrorMessage = "The Price is required")]
+        public int Price { get; set; }
+    }
+}
